f3cf1a51c9f537a40aab283da0fc9608e0425c97
TZsebastian <telmozurita23@gmail.com>
2026-07-28 19:01:17 -0500

    chore: estructura inicial del proyecto Spring Boot - Sebastian Zurita

---
+++ academic-events/src/test/java/ec/edu/ups/icc/academic_events/AcademicEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.icc.academic_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
